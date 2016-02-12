94b9fa9bbd4496f197c565c22862ebf9f5c9ac9f
mt_young <mt.young1012@gmail.com>
2016-02-12 21:36:14 +0900

    Pass→Pathに修正

--- copy_dir_structure.py
+++ copy_dir_structure.py
@@ -1,14 +1,14 @@
 import os
 
-inputDirPass = "C:\\Users\\***"
-outputDirPass = "C:\\Users\\***"
+inputDirPath = "C:\\Users\\***"
+outputDirPath = "C:\\Users\\***"
 
 #フォルダ構成を再帰的に取得
-for dpath, dnames, fnames in os.walk(inputDirPass):
+for dpath, dnames, fnames in os.walk(inputDirPath):
     for dname in dnames:
         #基準のフォルダからの相対パスを取得
-        relPath = os.path.relpath(dpath, inputDirPass)
+        relPath = os.path.relpath(dpath, inputDirPath)
         #カレントディレクトリを出力ディレクトリに戻してから相対パスへ移動
-        os.chdir(outputDirPass)
+        os.chdir(outputDirPath)
         os.chdir(relPath)
         os.mkdir(dname)
